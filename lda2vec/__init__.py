fe0c9fbbcce17c3457fecec2dfbcb40b7059299a
Rochan Avlur Venkat <rochan.avlur@gmail.com>
2018-05-31 17:06:39 +0000

    Fixed extra '+' before each line

--- lda2vec/__init__.py
+++ lda2vec/__init__.py
@@ -1,7 +1,7 @@
-+from .dirichlet_likelihood import dirichlet_likelihood
-+from .embed_mixture import EmbedMixture
-+from .tracking import Tracking
-+from .preprocess import tokenize
-+from .corpus import Corpus
-+from .topics import prepare_topics, print_top_words_per_topic, topic_coherence
-+from .negative_sampling import negative_sampling
+from .dirichlet_likelihood import dirichlet_likelihood
+from .embed_mixture import EmbedMixture
+from .tracking import Tracking
+from .preprocess import tokenize
+from .corpus import Corpus
+from .topics import prepare_topics, print_top_words_per_topic, topic_coherence
+from .negative_sampling import negative_sampling
